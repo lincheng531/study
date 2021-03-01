55f983a43f521f482a4f5931b769a4ca75cb9982
林城 <linc@kuaishang.cn>
2021-03-01 15:15:15 +0800

    项目初始化

--- study-jdk1_8/src/test/java/com/lincheng/study/jdk1_8/StudyJdk18ApplicationTests.java
+++ study-jdk1_8/src/test/java/com/lincheng/study/jdk1_8/StudyJdk18ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.lincheng.study.jdk1_8;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +8,7 @@ class StudyJdk18ApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
