686d42f408a7c66c7a84de1d43b23e4bc7c39393
林城 <linc@kuaishang.cn>
2021-04-19 14:39:49 +0800

    学习es

--- study-elasticsearch/src/test/java/com/lincheng/study/elasticsearch/ESIndexCreate.java
+++ study-elasticsearch/src/test/java/com/lincheng/study/elasticsearch/ESIndexCreate.java
@@ -4,6 +4,12 @@ import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
@@ -148,4 +154,96 @@ public class ESIndexCreate {
         //关闭es客户端
         esClient.close();
     }
+
+    @Test
+    public void testESDocGet() throws IOException {
+        //创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        //1.创建请求对象
+        GetRequest getRequest = new GetRequest();
+        getRequest.index("user").id("1001");
+        //2.客户端发送请求，获取响应对象
+        GetResponse getResponse = esClient.get(getRequest, RequestOptions.DEFAULT);
+        //3.打印结果信息
+        System.out.println("_index:" + getResponse.getIndex());
+        System.out.println("_type:" + getResponse.getType());
+        System.out.println("_id:" + getResponse.getId());
+        System.out.println("source:" + getResponse
+                .getSourceAsString());
+
+        //关闭es客户端
+        esClient.close();
+    }
+
+
+    @Test
+    public void testESDocDelete() throws IOException {
+        //创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        DeleteRequest deleteRequest = new DeleteRequest();
+        deleteRequest.index("user").id("1001");
+
+        DeleteResponse deleteResponse = esClient.delete(deleteRequest, RequestOptions.DEFAULT);
+
+        //打印信息
+        System.out.println(deleteResponse.toString());
+
+        //关闭es客户端
+        esClient.close();
+
+    }
+
+
+    @Test
+    public void testESDocInsertBatch() throws IOException {
+        //创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        //创建批量新增请求对象
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON,"name","张三","sex","女","age","31"));
+        bulkRequest.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON,"name","李四","sex","男","age","32"));
+        bulkRequest.add(new IndexRequest().index("user").id("1004").source(XContentType.JSON,"name","王五","sex","女","age","39"));
+
+        //客户端发送请求，获取响应对象
+        BulkResponse bulkResponse = esClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+        //打印结果信息
+        System.out.println("took:" + bulkResponse.getTook());
+        System.out.println("items:" + bulkResponse.getItems());
+        //关闭es客户端
+        esClient.close();
+    }
+
+    @Test
+    public void testESDocDeleteBatch() throws IOException {
+        //创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        //创建批量删除请求对象
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(new DeleteRequest().index("user").id("1002"));
+        bulkRequest.add(new DeleteRequest().index("user").id("1003"));
+        bulkRequest.add(new DeleteRequest().index("user").id("1004"));
+
+        //客户端发送请求，获取响应对象
+        BulkResponse bulkResponse = esClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+        //打印结果信息
+        System.out.println("took:" + bulkResponse.getTook());
+        System.out.println("items:" + bulkResponse.getItems());
+        //关闭es客户端
+        esClient.close();
+    }
+
 }
