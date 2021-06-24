d85f1dd310b4e4b43e2f85c851910bd1a17e0dee
lincheng5 <lincheng5@asiainfo.com>
2021-06-25 00:21:42 +0800

    集合

---
+++ study-basejava/src/main/java/com/lincheng/study/basejava/collection/StydyCollection.java
@@ -0,0 +1,12 @@
+package com.lincheng.study.basejava.collection;
+
+/**
+ * @author lincheng5
+ * @date 2021/6/25 0:21
+ */
+public class StydyCollection {
+
+    public static void main(String[] args) {
+
+    }
+}
