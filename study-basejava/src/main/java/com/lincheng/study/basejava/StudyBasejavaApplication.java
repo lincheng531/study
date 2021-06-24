68b4c199b19722a666b6cc9bfb7a767bac8db583
lincheng5 <lincheng5@asiainfo.com>
2021-06-24 16:51:56 +0800

    集合

---
+++ study-basejava/src/main/java/com/lincheng/study/basejava/StudyBasejavaApplication.java
@@ -0,0 +1,13 @@
+package com.lincheng.study.basejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBasejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBasejavaApplication.class, args);
+    }
+
+}
---
+++ study-basejava/src/test/java/com/lincheng/study/basejava/StudyBasejavaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lincheng.study.basejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = StudyBasejavaApplication.class)
+class StudyBasejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+}
