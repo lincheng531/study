41ea2e4eeecf88fce4c6cea67c83acd92ce73b7d
林城 <linc@kuaishang.cn>
2021-06-25 16:29:55 +0800

    线程

---
+++ study-basejava/src/main/java/com/lincheng/study/basejava/thread/StudyThread.java
@@ -0,0 +1,94 @@
+package com.lincheng.study.basejava.thread;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author linCheng
+ * @date 2021/6/25 14:05
+ *
+ * 一个进程能有多个线程
+ * 并行：单核CPU（切换进行执行）
+ * 并发：多核CPU（同时进行执行）
+ *
+ *
+ */
+public class StudyThread {
+
+
+    public static void main(String[] args) {
+
+        //getavailableProcessors();
+
+        buildThread();
+    }
+
+
+    /**
+     * 查询当前电脑CPU数
+     */
+    public static void getavailableProcessors(){
+        Runtime runtime= Runtime.getRuntime();
+        int i = runtime.availableProcessors();
+        System.out.println(i);
+    }
+
+
+    public static void buildThread(){
+        /**
+         * 创建线程的两种方式
+         *      1、继承Thread类，重写run方法。
+         *      2、实现Runnable接口，重写run方法。
+         *
+         * 当前主方法为主线程，而cal.start()为子方法。
+         *
+         *
+         *
+         */
+        cal cal = new cal();
+        cal.start();
+        //当main线程启动一个子线程 Thread-0，主线程不会阻塞，会继续执行
+
+        System.out.println("主线程继续执行：" + Thread.currentThread().getName());
+        for (int i = 0 ;i < 10 ;i++){
+            System.out.println("主线程：" + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * 当一个类继承了thread类，该类可以当做线程来使用
+     *
+     * 其实Thread也是实现了Runnable接口。
+     */
+    static class cal extends Thread{
+        int sun = 0;
+        @Override
+        public void run() {
+            //重写run方法，写上自己的业务逻辑
+            while (true) {
+                try {
+                    sun ++;
+                    Thread.sleep(1000);
+                    System.out.println("测试线程:" + sun + "线程名称:" + Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if(sun == 5){
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+}
