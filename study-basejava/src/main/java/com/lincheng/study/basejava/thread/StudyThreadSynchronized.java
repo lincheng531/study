e113845d57dc82031943814e2ee9f179c5a283a3
lincheng5 <lincheng5@asiainfo.com>
2021-06-27 23:17:13 +0800

    线程

--- study-basejava/src/main/java/com/lincheng/study/basejava/thread/StudyThreadSynchronized.java
+++ study-basejava/src/main/java/com/lincheng/study/basejava/thread/StudyThreadSynchronized.java
@@ -26,6 +26,16 @@ public class StudyThreadSynchronized {
      * public synchronized void m (String name){
      *      //需要被同步的代码
      * }
+     *
+     * 三、互斥锁。
+     *  基本介绍
+     * 1. Java语言中，引入了对象互斥锁的概念，来保证共享数据操作的完整性。
+     * 2.每个对象都对应于一一个可称为“互斥锁”的标记，这个标记用来保证在任一-时刻，只能有一个线程访问该对象。
+     * 3.关键字synchronized来与对象的互斥锁联系。当某个对象用synchronized修饰时，表明该对象在任一时刻只能由- 个线程访问
+     * 4.同步的局限性:导致程序的执行效率要降低
+     * 5.同步方法(非静态的)的锁可以是this,也可以是其他对象(要求是同一个对象)
+     * 6.同步方法(静态的)的锁为当前类本身。类
+     *
      **/
     public static void main(String[] args) {
         TestThreadSynchronized();
@@ -51,21 +61,24 @@ public class StudyThreadSynchronized {
         private int ticketNum = 100;
         private boolean loop = true;
 
-        public synchronized void sell(){
-            if (ticketNum <= 0){
-                loop = false;
-                System.out.println("售票结束----");
-                return;
+        //两种方式
+        public /*synchronized*/ void sell(){
+            synchronized (this) {
+                if (ticketNum <= 0) {
+                    loop = false;
+                    System.out.println("售票结束----");
+                    return;
+                }
+
+
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("窗口" + Thread.currentThread().getName() + "售出一张票" + "剩余票数=" + (--ticketNum));
             }
-
-
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("窗口"+ Thread.currentThread().getName() + "售出一张票" +"剩余票数=" + (--ticketNum));
         }
 
         @Override
