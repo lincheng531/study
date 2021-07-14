1bdef8cc37b897954a896fd14173210579f10d62
林城 <linc@kuaishang.cn>
2021-07-14 16:20:09 +0800

    并发，atomic

---
+++ study-basejava/src/main/java/com/lincheng/study/basejava/cas/StudyCAS.java
@@ -0,0 +1,87 @@
+package com.lincheng.study.basejava.cas;
+
+import com.lincheng.study.common.domain.alioss.vo.OssFileBusinessVO;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * @author linCheng
+ * @date 2021/7/14 14:01
+ */
+public class StudyCAS {
+
+
+    /**
+     * CAS并发原语体现在JAVA语言中就是sun.misc.Unsafe类中的各个方法。调用UnSafe类中的CAS方法， JVM会帮我们实现出CAS汇
+     * 编指令。这是一种完全依赖于硬件的功能，通过它实现了原子操作。再次强调，由于CAS是一 种系统原语，原语属于操作系统用语
+     * 范畴，是由若干条指令组成的，用于完成某个功能的一个过程，并且原语的执行必须是连续的，在执行过程中不允许被中断，也就
+     * 是说CAS是一条CPU的原子指令， 不会造成所谓的数据不一致问题。
+     */
+    public static void main(String[] args) {
+        //testABA();
+        testResolveABA();
+    }
+
+
+    private static void testResolveABA(){
+
+        AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<>(100, 1);
+
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println(Thread.currentThread().getName() + " 的版本号为：" + stamp);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            atomicStampedReference.compareAndSet(100, 101, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1 );
+            atomicStampedReference.compareAndSet(101, 100, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1 );
+        }).start();
+
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println(Thread.currentThread().getName() + " 的版本号为：" + stamp);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean b = atomicStampedReference.compareAndSet(100, 2019, stamp, stamp + 1);
+            System.out.println(b); // false
+            System.out.println(atomicStampedReference.getReference()); // 100
+        }).start();
+
+
+        System.out.println(atomicStampedReference.getReference());
+
+    }
+
+
+
+
+    private static void testABA(){
+        AtomicReference<Integer> atomicReference = new AtomicReference<>(100);
+
+        new Thread(() -> {
+            atomicReference.compareAndSet(100, 101);
+            atomicReference.compareAndSet(101, 100);
+        }).start();
+
+
+        new Thread(() -> {
+            // 保证上面线程先执行
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            atomicReference.compareAndSet(100, 2019);
+            System.out.println(atomicReference.get()); // 2019
+        }).start();
+
+        System.out.println(atomicReference.get());
+    }
+}
