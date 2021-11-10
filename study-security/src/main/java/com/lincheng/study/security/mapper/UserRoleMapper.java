d5dfaffa720dd1fb9ca3314cc2498bb9c05209a4
林城 <924093347@qq.com>
2021-11-10 16:25:05 +0800

    spring security

--- study-security/src/main/java/com/lincheng/study/security/mapper/UserRoleMapper.java
+++ study-security/src/main/java/com/lincheng/study/security/mapper/UserRoleMapper.java
@@ -17,7 +17,13 @@ import java.util.List;
 @Mapper
 public interface UserRoleMapper extends BaseMapper<UserRoleEntity> {
 
-
+    /**
+     * @Description: 通过用户id查询角色编码
+     * @author: linCheng
+     * @Date: 2021/11/10 16:23
+     * @param: custId
+     * @Return: java.util.List<java.lang.String>
+     */
     List<String> selectRoleCodesByCustId(Long custId);
 
 }
