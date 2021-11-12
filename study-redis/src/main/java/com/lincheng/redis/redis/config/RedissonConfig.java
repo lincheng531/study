cdf9385e20a167a86c4ca617803ed1d69dd76186
林城 <924093347@qq.com>
2021-11-12 16:13:06 +0800

    redis

---
+++ study-redis/src/main/java/com/lincheng/redis/redis/config/RedissonConfig.java
@@ -0,0 +1,26 @@
+package com.lincheng.redis.redis.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description:
+ * @author: linCheng
+ * @create: 2021-11-12 14:49
+ **/
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient getRedissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+
+
+}
--- study-redis/src/test/java/com/lincheng/redis/redis/StudyRedisApplicationTests.java
+++ study-redis/src/test/java/com/lincheng/redis/redis/StudyRedisApplicationTests.java
@@ -1,13 +1,110 @@
 package com.lincheng.redis.redis;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.*;
+import org.redisson.client.codec.StringCodec;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
-class StudyRedisApplicationTests {
+public class StudyRedisApplicationTests {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
 
     @Test
-    void contextLoads() {
+    public void string() {
+        RBucket<Object> bucket = redissonClient.getBucket("name", StringCodec.INSTANCE);
+        Object o = bucket.get();
+        System.out.println(o);
+
+        RBucket<Object> result = redissonClient.getBucket("studySetString", StringCodec.INSTANCE);
+        result.set("哈哈哈");
     }
 
+
+
+
+    @Test
+    public void hashes() {
+        //RMap<Object, Object> studySetString = redissonClient.getMap("studySetString", StringCodec.INSTANCE);
+        // 通过key获取RMap
+        RMap<String, String> studentRMap = redissonClient.getMap("studentMap");
+        // 设置map中key-value
+        studentRMap.put("id", "123");
+        studentRMap.put("name", "赵四");
+        studentRMap.put("age", "50");
+
+        // 设置key有效期
+        //studentRMap.expire(300, TimeUnit.SECONDS);
+        // 通过key获取value
+        RMap<Object, Object> studentMap = redissonClient.getMap("studentMap");
+        System.out.println(JSON.toJSONString(studentMap));
+        Object o = redissonClient.getMap("studentMap").get("name");
+        System.out.println(JSON.toJSONString(o));
+
+    }
+
+
+    @Test
+    public void lists() {
+        RList<Object> rList = redissonClient.getList("studentList");
+
+        rList.add(1);
+        rList.add(2);
+        rList.add("哈哈哈");
+
+
+        // 设置有效期
+        rList.expire(300, TimeUnit.SECONDS);
+        // 通过key获取value
+        Object studentList = redissonClient.getList("studentList").get(0);
+        System.out.println(JSON.toJSONString(studentList));
+        RList<Object> studentList1 = redissonClient.getList("studentList");
+        System.out.println(JSON.toJSONString(studentList1));
+    }
+
+
+    @Test
+    public void sets() {
+        RSet<Object> studentRSet = redissonClient.getSet("studentSet");
+        studentRSet.add("jack");
+        studentRSet.add("tom");
+        // 设置有效期
+        //studentRSet.expire(300, TimeUnit.SECONDS);
+        // 通过key获取value
+        redissonClient.getSet("studentSet");
+    }
+
+
+
+    @Test
+    public void sortedSets() {
+        RSortedSet<Object> studentSortedSet = redissonClient.getSortedSet("studentSortedSet");
+        studentSortedSet.add("jack");
+        studentSortedSet.add("tom");
+// 通过key获取value
+        redissonClient.getSortedSet("studentSortedSet");
+    }
+
+
+
+
+
+
+
 }
+
+
+
