4da5d7e9be587312a44d0474e5aa932ac75949d7
lincheng5 <lincheng5@asiainfo.com>
2021-11-14 23:12:50 +0800

    spring security auth2

---
+++ study-security-auth2/src/main/java/com/lincheng/study/auth2/StudySecurityAuth2Application.java
@@ -0,0 +1,13 @@
+package com.lincheng.study.auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySecurityAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySecurityAuth2Application.class, args);
+    }
+
+}
---
+++ study-security-auth2/src/test/java/com/lincheng/study/auth2/StudySecurityAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincheng.study.auth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySecurityAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
