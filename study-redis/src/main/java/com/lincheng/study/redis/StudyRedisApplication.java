b513d3de6c2626cd61b4d54c2f3d3b5b6ebaacad
林城 <924093347@qq.com>
2022-01-07 11:40:01 +0800

    redis

--- study-redis/src/main/java/com/lincheng/study/redis/StudyRedisApplication.java
+++ study-redis/src/main/java/com/lincheng/study/redis/StudyRedisApplication.java
@@ -2,8 +2,10 @@ package com.lincheng.study.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class StudyRedisApplication {
 
     public static void main(String[] args) {
