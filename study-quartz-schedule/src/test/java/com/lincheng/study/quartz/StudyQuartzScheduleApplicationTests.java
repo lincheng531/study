ef9d06fb90f6a9e7d801f4450496dc4855968387
林城 <924093347@qq.com>
2022-03-17 18:01:34 +0800

    定时任务

---
+++ study-quartz-schedule/src/test/java/com/lincheng/study/quartz/StudyQuartzScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincheng.study.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyQuartzScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
