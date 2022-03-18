f949284bae4b0fbb6591dd6b932492c64fe314b6
林城 <924093347@qq.com>
2022-03-18 14:39:16 +0800

    定时任务

--- study-quartz/src/main/java/com/lincheng/study/quartz/StudyQuartzApplication.java
+++ study-quartz/src/main/java/com/lincheng/study/quartz/StudyQuartzApplication.java
@@ -3,6 +3,7 @@ package com.lincheng.study.quartz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//数据库quartz-2.3.2.jar\org\quartz\impl\jdbcjobstore\tables_mysql.sql
 @SpringBootApplication
 public class StudyQuartzApplication {
 
