aa7ca185e6b93b3d25d11f3a83bb7b864b90318b
林城 <924093347@qq.com>
2022-03-25 15:55:39 +0800

    工作流

---
+++ study-activiti/src/main/java/com/lincheng/study/activiti/StudyActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.lincheng.study.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyActivitiApplication.class, args);
+    }
+
+}
---
+++ study-activiti/src/test/java/com/lincheng/study/activiti/StudyActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincheng.study.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
