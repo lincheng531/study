d8fdeda3933abcfa33ecc47adf4d39561286284c
林城(39588) <924093347@qq.com>
2022-07-18 15:04:13 +0800

    jdk8

--- study-jdk1_8/src/test/java/com/lincheng/study/jdk1_8/Stream/TestStream.java
+++ study-jdk1_8/src/test/java/com/lincheng/study/jdk1_8/Stream/TestStream.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -34,26 +35,27 @@ public class TestStream {
     );
 
 
-
     @Test
-    public void testForEach(){
+    public void testForEach() {
         userList.forEach(user -> System.out.println(JSON.toJSONString(user)));
     }
 
     @Test
-    public void testListObjectToListString(){
+    public void testListObjectToListString() {
         List<String> ids = userList.stream().map(User::getName).collect(Collectors.toList());
     }
 
 
     @Test
-    public void testSorted(){
+    public void testSorted() {
+        //排序
         List<User> users = userList.stream().sorted(Comparator.comparing(User::getAge)).collect(Collectors.toList());
         System.out.println(JSON.toJSONString(users));
     }
 
     @Test
-    public void testFilter(){
+    public void testFilter() {
+        //过滤
         List<User> users = userList.stream().filter(user -> user.getAge() > 30).collect(Collectors.toList());
 
         List<User> collect = userList.stream().filter(user -> user.getAge() != 11).collect(Collectors.toList());
@@ -63,7 +65,7 @@ public class TestStream {
 
 
     @Test
-    public void testLimit(){
+    public void testLimit() {
         // 从第三个开始截断，只输出前三个
         List<User> users = userList.stream().sorted(Comparator.comparing(User::getAge)).limit(3).collect(Collectors.toList());
         System.out.println(JSON.toJSONString(users));
@@ -71,21 +73,15 @@ public class TestStream {
 
 
     @Test
-    public void testSkip(){
+    public void testSkip() {
         // 跳过前三个元素，从第四个开始输出
         List<User> users = userList.stream().sorted(Comparator.comparing(User::getAge)).skip(3).collect(Collectors.toList());
         System.out.println(JSON.toJSONString(users));
     }
 
-    @Test
-    public void testDistinct(){
-        List<User> users = userList.stream().distinct().collect(Collectors.toList());
-        System.out.println(JSON.toJSONString(users));
-    }
-
 
     @Test
-    public void testNum(){
+    public void testNum() {
         IntSummaryStatistics num = userList.stream().mapToInt(User::getAge).summaryStatistics();
         System.out.println("总共人数：" + num.getCount());
         System.out.println("平均年龄：" + num.getAverage());
@@ -112,7 +108,6 @@ public class TestStream {
     }
 
 
-
     @Test
     public void testFlatMap() {
         //对每个元素执行mapper指定的操作，并用所有mapper返回的Stream中的元素组成一个新的Stream作为最终返回结果
@@ -128,12 +123,11 @@ public class TestStream {
 
 
     @Test
-    public void testFindFirst(){
+    public void testFindFirst() {
         //获取第一个元素.
         User user = userList.stream().findFirst().orElse(null);
         System.out.println(JSON.toJSONString(user));
     }
-
 
 
     @Test
@@ -150,7 +144,6 @@ public class TestStream {
         //min：返回流中最小值
 
 
-
         //找到年龄最大的
         User user = userList.stream().reduce((s1, s2) -> s1.getAge() > s2.getAge() ? s1 : s2).orElse(null);
         System.out.println(JSON.toJSONString(user));
@@ -161,11 +154,11 @@ public class TestStream {
 
 
         List<Employee> emps = Arrays.asList(
-                new Employee("张三", 18, 9999.9,2),
-                new Employee("李四", 22, 6666.9,1),
-                new Employee("王五", 23, 8888.9,1),
-                new Employee("赵六", 26, 7777.9,1),
-                new Employee("田七", 20, 5555.9,1)
+                new Employee("张三", 18, 9999.9, 2),
+                new Employee("李四", 22, 6666.9, 1),
+                new Employee("王五", 23, 8888.9, 1),
+                new Employee("赵六", 26, 7777.9, 1),
+                new Employee("田七", 20, 5555.9, 1)
         );
 
         // 全部 Employee 是 Free，就会返回true
@@ -186,7 +179,7 @@ public class TestStream {
 
         Optional<Employee> emp1 = emps.stream()
                 .findFirst();
-                System.out.println(emp1.get());
+        System.out.println(emp1.get());
 
         // 如果是数据较少，串行地情况下，一般会返回第一个结果
         // 如果是并行的情况，那就不能确保是第一个。
@@ -203,27 +196,27 @@ public class TestStream {
 
 
         List<Employee> emps = Arrays.asList(
-                new Employee(1,"张三", 18, 1.0,2),
-                new Employee(1,"张三", 18, 2.0,2),
-                new Employee(1,"张三", 18, 3.0,2),
-                new Employee(2,"李四", 22, 6666.9,1),
-                new Employee(3,"王五", 23, 8888.9,1),
-                new Employee(4,"赵六", 26, 7777.9,1),
-                new Employee(5,"田七", 20, 5555.9,1)
+                new Employee(1, "张三", 18, 1.0, 2),
+                new Employee(1, "张三", 18, 2.0, 2),
+                new Employee(1, "张三", 18, 3.0, 2),
+                new Employee(2, "李四", 22, 6666.9, 1),
+                new Employee(3, "王五", 23, 8888.9, 1),
+                new Employee(4, "赵六", 26, 7777.9, 1),
+                new Employee(5, "田七", 20, 5555.9, 1)
         );
 
         // 使用Collectors.joining()拼接字符串
-        Stream<String> stream = Stream.of("张三","李四","王五","赵六");
+        Stream<String> stream = Stream.of("张三", "李四", "王五", "赵六");
         String s = stream.collect(Collectors.joining("-", "(", ")"));
         System.out.println(s);
-        String s1 =  emps.stream().map(Employee::getName).collect(Collectors.joining(";"));
+        String s1 = emps.stream().map(Employee::getName).collect(Collectors.joining(";"));
         System.out.println(s1);
 
 
         List<Employee> employeeList = new ArrayList<>();
         Map<Integer, List<Employee>> checkMap = emps.stream().collect(Collectors.groupingBy(Employee::getId));
-        checkMap.forEach((key,value)->{
-            value.stream().reduce((a,b) -> new Employee(a.getId(),a.getName(),a.getAge(),a.getSalary() + b.getSalary(),a.getStatus())).ifPresent(employeeList::add);
+        checkMap.forEach((key, value) -> {
+            value.stream().reduce((a, b) -> new Employee(a.getId(), a.getName(), a.getAge(), a.getSalary() + b.getSalary(), a.getStatus())).ifPresent(employeeList::add);
         });
         System.out.println(JSON.toJSONString(employeeList));
 
@@ -256,14 +249,13 @@ public class TestStream {
         // 最大值、最小值，也可以收集
         // 举一反三。。。略
 
-        Map<String, Integer> mapKeyValue = emps.stream().collect(Collectors.toMap(Employee :: getName, Employee :: getAge, (value1, value2 )-> value2));
+        Map<String, Integer> mapKeyValue = emps.stream().collect(Collectors.toMap(Employee::getName, Employee::getAge, (value1, value2) -> value2));
         System.out.println(JSON.toJSONString(mapKeyValue));
 
-
+        // 一级分组
         Map<String, Employee> mapKeyObject = emps.stream().collect(Collectors.toMap(Employee::getName, a -> a, (k1, k2) -> k1));
         System.out.println(JSON.toJSONString(mapKeyObject));
 
-        // 一级分组
         Map<Integer, List<Employee>> map = emps.stream().collect(Collectors.groupingBy(Employee::getStatus));
         System.out.println(map);
 
@@ -281,6 +273,7 @@ public class TestStream {
 
 
         Map<Integer, Map<String, Employee>> collect = emps.stream().collect(Collectors.groupingBy(Employee::getAge, Collectors.toMap(Employee::getName, a -> a, (k1, k2) -> k1)));
+        Map<Integer, Map<String, List<Employee>>> collect4 = emps.stream().collect(Collectors.groupingBy(Employee::getAge, Collectors.groupingBy(Employee::getName)));
 
         // 三级分组
         Map<Integer, Map<Integer, Map<String, Employee>>> collect2 = emps.stream().collect(Collectors.groupingBy(Employee::getAge, Collectors.groupingBy(Employee::getStatus, Collectors.toMap(Employee::getName, a -> a, (k1, k2) -> k1))));
@@ -289,6 +282,14 @@ public class TestStream {
         List<Employee> collect3 = emps.stream().filter(distinctByKey(Employee::getAge)).collect(Collectors.toList());
         System.out.println(JSON.toJSONString(collect3));
 
+        //对象去重
+        List<Employee> collect6 = emps.stream().distinct().collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(collect6));
+
+        //数组去重
+        List<String> collect5 = emps.stream().map(Employee::getName).distinct().collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(collect5));
+
 
         // 分区（分成两部分）
         Map<Boolean, List<Employee>> listMap = emps.stream()
@@ -296,13 +297,28 @@ public class TestStream {
         System.out.println(listMap);
 
         //总结（方便地获取多种数据统计方式的结果）
-        DoubleSummaryStatistics dss = emps.stream()
-                .collect(Collectors.summarizingDouble(Employee::getSalary));
+        DoubleSummaryStatistics dss = emps.stream().collect(Collectors.summarizingDouble(Employee::getSalary));
         System.out.println(dss.getMax());
         System.out.println(dss.getMin());
         System.out.println(dss.getSum());
         System.out.println(dss.getCount());
         System.out.println(dss.getAverage());
+
+
+        IntSummaryStatistics num = userList.stream().mapToInt(User::getAge).summaryStatistics();
+        System.out.println(num.getMax());
+        System.out.println(num.getMin());
+        System.out.println(num.getSum());
+        System.out.println(num.getCount());
+        System.out.println(num.getAverage());
+
+
+        //获取最小年龄的对象
+        Employee employeeMin = emps.stream().min(Comparator.comparingLong(Employee::getAge)).orElse(new Employee());
+        System.out.println(JSON.toJSONString(employeeMin));
+        //获取最大年龄的对象
+        Employee employeeMax = emps.stream().max(Comparator.comparingLong(Employee::getAge)).orElse(new Employee());
+        System.out.println(JSON.toJSONString(employeeMax));
 
     }
 
@@ -329,7 +345,6 @@ public class TestStream {
                 .forEach(System.out::println); // 终止操作
 
 
-
         // （2）生成
         Stream.generate(() -> new Random().nextInt(32))
                 .limit(32)
@@ -338,7 +353,7 @@ public class TestStream {
     }
 
     public static void main(String[] args) {
-        List<String> list=new ArrayList<String>();
+        List<String> list = new ArrayList<String>();
         list.add("A");
         list.add("B");
         list.add("C");
@@ -349,15 +364,32 @@ public class TestStream {
         System.out.println(s);
 
         HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("1","1");
-        hashMap.put("2","2");
-        hashMap.put("3","3");
+        hashMap.put("1", "1");
+        hashMap.put("2", "2");
+        hashMap.put("3", "3");
 
     }
 
 
     @Test
     public void testBigDecimal() {
+
+
+        //常用
+        //2.4
+        System.out.println("四舍五入(保留位后一位是5进位) = " + new BigDecimal("2.35").setScale(1, BigDecimal.ROUND_HALF_UP));
+        //2.3
+        System.out.println("四舍五入(保留位后一位是5舍弃) = " + new BigDecimal("2.35").setScale(1, BigDecimal.ROUND_HALF_DOWN));
+        //4.6
+        //向最接近数字方向舍入的舍入模式，如果与两个相邻数字的距离相等，则向相邻的偶数舍入。
+        System.out.println("四舍五入(保留位是奇数) = " + new BigDecimal("4.55").setScale(1, BigDecimal.ROUND_HALF_EVEN));
+        //4.4
+        //向最接近数字方向舍入的舍入模式，如果与两个相邻数字的距离相等，则向相邻的偶数舍入。
+        System.out.println("四舍五入(保留位是偶数) = " + new BigDecimal("4.45").setScale(1, BigDecimal.ROUND_HALF_EVEN));
+        // 2.4
+        System.out.println("进位= " + new BigDecimal("2.31").setScale(1, BigDecimal.ROUND_UP));
+
+
         BigDecimal bignum1 = new BigDecimal("10");
         BigDecimal bignum2 = new BigDecimal("5");
         BigDecimal bignum3 = null;
@@ -375,35 +407,34 @@ public class TestStream {
         System.out.println("积  是：" + bignum3);
 
         //除法
-        bignum3 = bignum1.divide(bignum2);
+        bignum3 = bignum1.divide(bignum2, 6, RoundingMode.HALF_UP);
         System.out.println("商  是：" + bignum3);
 
 
         BigDecimal a = new BigDecimal("0");
         BigDecimal b = new BigDecimal("0.0");
         //前提为a、b均不能为null
-        if(a.compareTo(b) == -1){
+        if (a.compareTo(b) == -1) {
             System.out.println("a小于b");
         }
 
-        if(a.compareTo(b) == 0){
+        if (a.compareTo(b) == 0) {
             System.out.println("a等于b");
         }
 
-        if(a.compareTo(b) > 0){
+        if (a.compareTo(b) > 0) {
             System.out.println("a大于b");
         }
 
-        if(a.compareTo(b) > -1){
+        if (a.compareTo(b) > -1) {
             System.out.println("a大于等于b");
         }
 
-        if(a.compareTo(b) < 1){
+        if (a.compareTo(b) < 1) {
             System.out.println("a小于等于b");
         }
 
     }
-
 
 
     //去重
