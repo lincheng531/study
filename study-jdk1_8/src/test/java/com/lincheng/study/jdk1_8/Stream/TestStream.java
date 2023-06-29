863ce0d2b4cfae90241d31849301277ab8b79423
林城 <924093347@qq.com>
2023-06-29 14:07:45 +0800

    jkd8

--- study-jdk1_8/src/test/java/com/lincheng/study/jdk1_8/Stream/TestStream.java
+++ study-jdk1_8/src/test/java/com/lincheng/study/jdk1_8/Stream/TestStream.java
@@ -336,6 +336,36 @@ public class TestStream {
 
     }
 
+    @Test
+    public void flatMapTest() {
+            List<List<Integer>> lists = new ArrayList<>();
+            List<Integer> arrayList = new ArrayList<>();
+            arrayList.add(1);
+            arrayList.add(3);
+            arrayList.add(22);
+            arrayList.add(11);
+            arrayList.add(44);
+            arrayList.add(null);
+            arrayList.add(4);
+            lists.add(arrayList);
+
+            List<Integer> arrayList2 = JSON.parseArray(JSON.toJSONString(arrayList), Integer.class);
+            arrayList2.add(55);
+            lists.add(arrayList2);
+
+            List<Integer> arrayList3 = JSON.parseArray(JSON.toJSONString(arrayList), Integer.class);
+            arrayList3.add(99);
+            arrayList3.add(-2);
+            lists.add(arrayList3);
+
+
+            //将子集合抽上来形成一个大集合
+            List<Integer> collect = lists.stream().flatMap(item -> item.stream().filter(Objects::nonNull))
+                    .collect(Collectors.toList());
+
+            System.out.println(collect);
+    }
+
 
     @Test
     public void foundStream() {
